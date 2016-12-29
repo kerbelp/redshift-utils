9334fb0c12fd6610cfb392a5b399ad51975c18bf
Pavel Kerbel <pavel.kerbel@ironsrc.com>
2016-12-30 00:48:58 +0200

    added comment

--- v_capacity_usage.sql
+++ v_capacity_usage.sql
@@ -1,3 +1,13 @@
+/*
+A view for capacity usage 
+
+Example result: 
+
+capacity_gbytes | used_gbytes | free_gbytes |
+----------------|-------------|-------------|
+      17827     |     10789   |    7037     |
+----------------|-------------|-------------|
+*/
 select
   sum(capacity) / 1024 as capacity_gbytes, 
   sum(used) / 1024 as used_gbytes, 
