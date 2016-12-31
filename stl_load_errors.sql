f8535e1940e629ded54713623e7711f7339ffc74
Pavel Kerbel <pavel.kerbel@ironsrc.com>
2016-12-31 13:25:55 +0200

    added stl_load_errors.sql

---
+++ stl_load_errors.sql
@@ -0,0 +1,27 @@
+ /*
+ A query displaying stl_load_errors by day.
+ 
+ result example:
+ 
+
+     day    | colname | err_reason  |    filename    |  amount
+------------+---------+-------------+----------------+---------+
+ 2016-12-31 | column  | Overflow... | s3://file_path |    10
+
+*/
+
+SELECT TRUNC(starttime) as day,
+       colname,
+       err_reason,
+       filename,
+       COUNT(*) as amount
+FROM stl_load_errors
+WHERE filename LIKE '%s3_file_path%'
+GROUP BY day,
+         colname,
+         err_reason,
+         filename
+ORDER BY day,
+         colname,
+         err_reason,
+         filename;
