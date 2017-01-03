7bc4de3d97c9055552a203640cad3e1ed8a94b67
Pavel Kerbel <pavel.kerbel@ironsrc.com>
2017-01-04 00:41:42 +0200

    changed v_users_groups

--- v_users_groups.sql
+++ v_users_groups.sql
@@ -4,29 +4,19 @@
  example:
  select * from admin.v_users_groups;
 
-+----------+------------+-----------------+
-| username |  usesuper  |      group      |
-+----------+------------+-----------------+
-|   admin  |    true    |     group_a     |
-+----------+------------+-----------------+
++------------+-------------+--------------+
+| group_name |  user_name  | is_superuser |
++------------+-------------+--------------+
+|   group_a  |    user_a   |     true     |
++------------+-------------+--------------+
 
 */
 
 CREATE OR REPLACE VIEW admin.v_users_groups 
 AS
-SELECT derived_table1.username,
-       derived_table1.usesuper,
-       derived_table1."group"
-FROM (SELECT u.usename AS username,
-             u.usesuper,
-             g.groname AS "group"
-      FROM pg_group g
-        LEFT JOIN pg_user u ON ( (',' || ARRAY_TO_STRING (g.grolist,',')) || ',') ~~ ( ('%,' || u.usesysid) || ',%')
-      UNION
-      SELECT u.usename AS username,
-             u.usesuper,
-             g.groname AS "group"
-      FROM pg_group g
-        RIGHT JOIN pg_user u ON ( (',' || ARRAY_TO_STRING (g.grolist,',')) || ',') ~~ ( ('%,' || u.usesysid) || ',%')) derived_table1
-ORDER BY derived_table1.username,
-         derived_table1."group";
+SELECT pg_group.groname as group_name,
+       pg_user.usename as user_name,
+       pg_user.usesuper as is_superuser
+FROM pg_group,
+     pg_user
+WHERE pg_user.usesysid = ANY (pg_group.grolist);
